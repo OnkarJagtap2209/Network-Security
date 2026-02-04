08ad1e8b1f134ae71756bf8a53447d0a2e20d140
onkarJagtap2209 <onkarofficial2209@gmail.com>
2026-02-04 12:54:57 +0530

    Data ingestion component

--- networksecurity/exception/exception.py
+++ networksecurity/exception/exception.py
@@ -5,7 +5,7 @@ class NetworkSecurityException(Exception):
     def __init__(self,error_message,error_details:sys):
         self.error_message = error_message
         _,_,exc_tb = error_details.exc_info()
-        
+    
         self.lineno=exc_tb.tb_lineno
         self.file_name=exc_tb.tb_frame.f_code.co_filename 
     
@@ -15,7 +15,7 @@ class NetworkSecurityException(Exception):
         
 if __name__=='__main__':
     try:
-        logger.logging.INFO("Enter the try block")
+        logger.logging.info("Enter the try block")
         a=1/0
         print("This will not be printed",a)
     except Exception as e:
